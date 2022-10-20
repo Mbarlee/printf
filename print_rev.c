de307e94b906242230b06403c7b38fb2e9640ad2
Teboho <teboho.mofokeng@hotmail.com>
2022-10-20 03:00:39 -0700

    printf git commit script

--- print_rev.c
+++ print_rev.c
@@ -7,21 +7,19 @@
  */
 int print_rev(char *str)
 {
-	char *z = str;
-	int i;
-	int count1 = 0;
-	int count = 0;
-
-	while (*str != '\0')
-	{
-		str++;
-		count1++;
-	}
-
-	i = count1 - 1;
-	while (i >= 0)
-	{
-		_putchar(*(z + i));
-		i--;
-	}
+char *z = str;
+int i;
+int count1 = 0;
+int count = 0;
+while (*str != '\0')
+{
+str++;
+count1++;
+}
+i = count1 - 1;
+while (i >= 0)
+{
+_putchar(*(z + i));
+i--;
+}
 }
